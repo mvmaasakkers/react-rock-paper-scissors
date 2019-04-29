ebe93f3ed581ebbf94254dbc87af7bf2e016f6d8
Martijn van Maasakkers <martijn@newestindustry.nl>
2019-04-29 10:48:11 +0200

    Updates

---
+++ precache-manifest.5dfd5b9121f22bab418eefc89b239637.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8951e446b18a9802bd28ed646a563a86",
+    "url": "/react-rock-paper-scissors/index.html"
+  },
+  {
+    "revision": "d103962cae369763614c",
+    "url": "/react-rock-paper-scissors/static/css/main.89596915.chunk.css"
+  },
+  {
+    "revision": "d4d41c5f5bbc793c0a39",
+    "url": "/react-rock-paper-scissors/static/js/2.e7d566f1.chunk.js"
+  },
+  {
+    "revision": "d103962cae369763614c",
+    "url": "/react-rock-paper-scissors/static/js/main.e060d055.chunk.js"
+  },
+  {
+    "revision": "ea0328a1b122e0e7f6fd",
+    "url": "/react-rock-paper-scissors/static/js/runtime~main.e820fcee.js"
+  }
+]);
